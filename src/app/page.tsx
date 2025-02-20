fc77f7d1735cc3489138e3ce5b86b1237d8340be
imad273 <abbadimad0123@gmail.com>
2025-02-20 09:37:22 +0100

    add listing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,55 @@
 import Image from "next/image";
+
 import hero from "@/assets/hero.png";
+import Home1 from "@/assets/home-1.jpg";
+import Home2 from "@/assets/home-2.jpg";
+import Home3 from "@/assets/home-3.jpg";
+import Home4 from "@/assets/home-4.jpg";
+
 import { Button } from "@/components/ui/button";
 import { SiHomeassistant } from "react-icons/si";
 import { MdAddHome, MdSell } from "react-icons/md";
-import { FaFileContract } from "react-icons/fa";
+import { FaArrowRight, FaFileContract } from "react-icons/fa";
+import { IoBedSharp, IoLocationSharp } from "react-icons/io5";
+import { PiBathtubFill } from "react-icons/pi";
 
 export default function Home() {
+
+  const listings = [
+    {
+      id: 1,
+      image: Home1,
+      name: "The Queen Inside - Type 1",
+      location: "Dubai Marina",
+      beds: 3,
+      baths: 2,
+    },
+    {
+      id: 3,
+      image: Home2,
+      name: "The Queen Inside - Type 2",
+      location: "Dubai Marina",
+      beds: 3,
+      baths: 2,
+    },
+    {
+      id: 4,
+      image: Home3,
+      name: "Willow Green",
+      location: "Emirates Hills",
+      beds: 4,
+      baths: 2,
+    },
+    {
+      id: 2,
+      image: Home4,
+      name: "Modern Sea",
+      location: "Palm Jumeirah",
+      beds: 5,
+      baths: 3,
+    },
+  ]
+
   return (
     <main>
       <section className="bg-gradient-to-tr from-[#C8E2E9] to-[#F7DCBE]">
@@ -70,7 +114,76 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="h-screen"></section>
+      <section>
+        <div className='h-full md:h-[30vh] bg-slate-800'>
+          <div className="container grid md:grid-cols-3 gap-8 md:gap-0 text-white h-full py-6 md:py-0">
+            <div className='flex flex-col justify-center items-center h-full'>
+              <h3 className='text-4xl font-semibold mb-2 md:mb-3'>
+                +90
+              </h3>
+              <p>
+                Properties Sold
+              </p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <h3 className='text-4xl font-semibold mb-2 md:mb-3'>
+                +12
+              </h3>
+              <p>
+                Homes Managed
+              </p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <h3 className='text-4xl font-semibold mb-2 md:mb-3'>
+                100%
+              </h3>
+              <p>
+                Client Satisfaction Rate
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="h-full container py-10">
+        <div className="pt-6 pb-3">
+          <h2 className="text-slate-900 text-3xl font-bold">Our recent listing</h2>
+        </div>
+
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-5">
+          {listings.map(listing => (
+            <div key={listing.id} className="border rounded-md flex flex-col">
+              <Image src={listing.image} alt={"home image 1"} className="rounded-md p-1 cursor-pointer" />
+
+              <div className="flex flex-col h-full">
+
+                <div className="pb-3 px-2 flex-1">
+                  <h3 className="font-semibold text-slate-900 mt-3">{listing.name}</h3>
+                  <div className="mt-3 flex gap-2 items-center text-sm text-neutral-500 font-semibold">
+                    <IoLocationSharp />
+                    <p>{listing.location}</p>
+                  </div>
+                  <div className="flex gap-3 mt-1 items-center text-sm">
+                    <div className="flex items-center gap-1.5 text-neutral-500 font-semibold">
+                      <IoBedSharp />
+                      <span>{listing.beds}</span>
+                    </div>
+                    <div className="flex items-center gap-1.5 text-neutral-500 font-semibold">
+                      <PiBathtubFill />
+                      <span>{listing.baths}</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="hover:text-stone-900 duration-200 flex justify-center items-center gap-2 text-sm p-2 cursor-pointer font-semibold text-slate-900">
+                  <p>See Details</p>
+                  <FaArrowRight />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
